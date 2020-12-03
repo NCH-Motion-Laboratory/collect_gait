cbb8d5d57c8997dedd2491eea54af2b8739715f8
Nurminen Jussi <jnu@iki.fi>
2020-12-03 14:26:38 +0200

    add numpy collect

--- testbench.py
+++ testbench.py
@@ -12,6 +12,7 @@ from collect_gait import utils
 import datetime
 import gaitutils
 import itertools
+import matplotlib.pyplot as plt
 
 # let's get all sessions under this dir...
 rootdir = r"Z:\Userdata_Vicon_Server"
@@ -26,16 +27,28 @@ dirs = utils.get_sessiondirs(rootdir, newer_than=date, substrings=substrings)
 
 
 # %% scan the directory tree
-alldata = dict()
+alldirs = dict()  # save dirs/files here
 
-for d in dirs:
+for i, d in enumerate(dirs):
     print(f'reading from {d}')
     c3ds = gaitutils.sessionutils.get_c3ds(d, tags=tags, trial_type='dynamic')
     print(f'{len(c3ds)} tagged trials')
-    alldata[d] = c3ds
+    alldirs[d] = c3ds
+    # XXX: let's limit the number of dirs for testing purposes
+    if i == 5:
+        break
 
-print(f'{len(d)} gait session dirs')
-allfiles = list(itertools.chain.from_iterable(alldata.values()))
+print('---')
+print(f'{i} gait session dirs')
+allfiles = list(itertools.chain.from_iterable(alldirs.values()))
 print(f'{len(allfiles)} total c3d files')
 
 
+# %% load the trials and get some curves
+trials = [gaitutils.trial.Trial(fn) for fn in allfiles]
+
+# collect the data into numpy arrays
+# all model variables will be normalized into gait cycles (x axis 0..100%)
+data_all, ncycles = gaitutils.stats.collect_trial_data(trials)
+knee_flex = data_all['model']['LKneeAnglesX']  # extract left sagittal knee angle
+plt.plot(knee_flex.T)  # matplotlib plots columns by default, so transpose
