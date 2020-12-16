0d0bbc04b094df0f6b3256965cfe7ae84ff9a5f3
Jussi Nurminen <jnu@iki.fi>
2020-12-16 17:39:48 +0200

    wip: histograms

--- try_plot_extracted.py
+++ try_plot_extracted.py
@@ -3,6 +3,18 @@
 
 plot histograms from extracted values
 
+TODO:
+
+need 2 representative sessions (with some differences?)
+they need to be separated (currently everything goes into one hist)
+session names -> legend
+ylabels, xlabels
+L/R column titles
+avg/stddev lines?
+JP defined vars
+x axis scaling
+
+
 @author: Jussi (jnu@iki.fi)
 """
 
@@ -21,21 +33,24 @@ import plotly.graph_objects as go
 
 # let's get all sessions under this dir...
 rootdir = r"Z:\Userdata_Vicon_Server"
+rootdir = r"C:\Temp\D0063_RR"
 # ...newer than this date
-date = datetime.datetime(2019, 3, 1)
+date = datetime.datetime(2018, 1, 1)
 # tags for dynamic trials
 tags = ['E1', 'E2', 'E3', 'T1', 'T2', 'T3']
 # strings that must be included in dir names
 substrings = ['1_Diplegia', '1_Hemiplegia', '1_Eridiagnoosit', '1_Meningomyelecele']
+substrings = None
 
-dirs = utils.get_sessiondirs(rootdir, newer_than=date, substrings=substrings)
 
 
 # %% scan the directory tree
 alldirs = dict()  # save dirs/files here
-MAX_DIRS = 15  # XXX: let's limit the number of dirs for testing purposes
+MAX_DIRS = 2  # XXX: let's limit the number of dirs for testing purposes
 
-for row, d in enumerate(dirs):
+dirs = utils.get_sessiondirs(rootdir, newer_than=date, substrings=substrings)
+
+for row, d in enumerate(dirs, 1):
     print(f'reading from {d}')
     c3ds = gaitutils.sessionutils.get_c3ds(d, tags=tags, trial_type='dynamic')
     print(f'{len(c3ds)} tagged trials')
@@ -53,7 +68,9 @@ print(f'{len(allfiles)} total c3d files')
 data_all, cycles_all = gaitutils.stats.collect_trial_data(allfiles)
 
 extr_vals = dict()
-for var in list(gaitutils.models.pig_lowerbody.varnames) + list(gaitutils.models.pig_lowerbody_kinetics.varnames):
+for var in list(gaitutils.models.pig_lowerbody.varnames) + list(
+    gaitutils.models.pig_lowerbody_kinetics.varnames
+):
     data = data_all['model'][var]
     toeoffs = [cyc.toeoffn for cyc in cycles_all['model'][var]]
     print(var)
@@ -86,34 +103,39 @@ halutut muuttujat:
 # these are lists of (nested) keys into the dict returned by curve_extract_values
 
 nested_keylist = [
-['AnkleAnglesX', 'contact'],
-['KneeAnglesX', 'contact'],
-['HipAnglesX', 'contact'],
-['AnkleAnglesX', 'peaks', 'stance', 'min'],
-['KneeAnglesX', 'peaks', 'stance', 'max'],
-['HipAnglesX', 'peaks', 'stance', 'min'],
-['KneeAnglesX', 'peaks', 'swing', 'max'],
-# ['KneeAnglesX', 'peaks', 'swing', 'max'],
-['HipAnglesX', 'peaks', 'swing', 'max'],
+    ['AnkleAnglesX', 'contact'],
+    ['KneeAnglesX', 'contact'],
+    ['HipAnglesX', 'contact'],
+    ['AnkleAnglesX', 'peaks', 'stance', 'min'],
+    ['KneeAnglesX', 'peaks', 'stance', 'max'],
+    ['HipAnglesX', 'peaks', 'stance', 'min'],
+    ['KneeAnglesX', 'peaks', 'swing', 'max'],
+    # ['KneeAnglesX', 'peaks', 'swing', 'max'],
+    ['HipAnglesX', 'peaks', 'swing', 'max'],
 ]
 
 
 def _compose_varname(nested_keys):
-    """Compose a variable name for extracted variable"""
+    """Compose a variable name for extracted variable.
+    
+    E.g. ['HipAnglesX', 'peaks', 'swing', 'max']
+    -> 'Hip flexion maximum during swing phase'
+    """
     varname = nested_keys[0]
+    # get variable description from gaitutils.models
     themodel = gaitutils.models.model_from_var(varname)
-    name = themodel.varlabels_noside[varname]  # variable label
-    name += ' at '
+    name = themodel.varlabels_noside[varname]
     if nested_keys[1] == 'contact':
-        name += 'initial contact'
+        name += ' at initial contact'
     elif nested_keys[1] in ['peaks', 'extrema']:
-        phase = nested_keys[2]
-        name += phase + ' phase '
-        name += nested_keys[3]
+        phase = nested_keys[2]  # swing, stance etc.
+        valtype = nested_keys[3]  # min, max etc.
+        val_trans = {'max': 'maximum', 'min': 'minimum'}
+        name += ', %s phase %s' % (phase, val_trans[valtype])
     return name
 
 
-def nested_get(di, keys):    
+def nested_get(di, keys):
     """Get a value from a nested dict, using a list of keys"""
     for key in keys:
         di = di[key]  # iterate until we exhaust the nested keys
@@ -129,25 +151,32 @@ for ctxt in 'RL':
         nested_get(extr_vals, nested_keys_context)
 
 
-
-
-# %%
+# %% histogram proof of concept
+#
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
 ctxts = ['Left', 'Right']
-subtitles = [f'{_compose_varname(nested_keys)}' for nested_keys in nested_keylist for ctxt in ctxts]
+subtitles = [
+    f'{_compose_varname(nested_keys)}'
+    for nested_keys in nested_keylist
+    for ctxt in ctxts
+]
 
 nvars = len(nested_keylist)
 fig = make_subplots(rows=nvars, cols=2, subplot_titles=subtitles)
+
+ctxt_colors = {'L': 'red', 'R': 'blue'}
 
 for row, nested_keys in enumerate(nested_keylist, 1):
     varname = _compose_varname(nested_keys)
     for col, ctxt in enumerate('LR', 1):
         nested_keys_ctxt = nested_keys.copy()
-        nested_keys_ctxt[0] = ctxt + nested_keys[0] 
+        nested_keys_ctxt[0] = ctxt + nested_keys[0]
         vals = nested_get(extr_vals, nested_keys_ctxt)
-        hist = go.Histogram(x=vals, nbinsx=20, name='', opacity=.5)
+        hist = go.Histogram(
+            x=vals, nbinsx=20, name='', opacity=0.5, marker_color=ctxt_colors[ctxt]
+        )
         fig.append_trace(hist, row=row, col=col)
 
 # Overlay both histograms
@@ -159,12 +188,12 @@ gaitutils.viz.plot_misc.show_fig(fig)
 
 
 # %%
-a = np.array([[[1,2,3], [1,2,3]],
-              [[999,5,6], [4,5,6]],
-              [[999,8,9], [7,999,9]]
-              ])
 
-#for x in a:
+a = np.array(
+    [[[1, 2, 3], [1, 2, 3]], [[999, 5, 6], [4, 5, 6]], [[999, 8, 9], [7, 999, 9]]]
+)
+
+# for x in a:
 #    print(x)
 
 np.delete(a, np.where(a == 999)[0], axis=0)
@@ -173,6 +202,6 @@ np.delete(a, np.where(a == 999)[0], axis=0)
 # %%
 
 
-for i in range(0,len(a)):
+for i in range(0, len(a)):
     if 999 in a[i]:
         np.delete(a, i, 0)
